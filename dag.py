09c6c0dd1b7a72c3e008094a531fe21b73a78ebc
CAO Song Toan <toancs272@gmail.com>
2022-02-15 16:37:03 +0100

    update DAG

--- dag.py
+++ dag.py
@@ -14,9 +14,9 @@ YESTERDAY = '{{ macros.ds_format(macros.ds_add(ds, 1), "%Y-%m-%d", "%d") }}'
 
 dag = DAG(
     # TODO : compléter le nom du DAG avec votre nom de gorupe dedans
-    dag_id='',
+    dag_id='circulation_group9',
     # TODO : compléter le schedule interval
-    schedule_interval='',
+    schedule_interval='@daily',
     max_active_runs=1,
     start_date=datetime(2022, 2, 14),
 )
@@ -24,7 +24,7 @@ dag = DAG(
 @task(task_id='download_raw_data', dag=dag)
 def download_raw_data(year, month, day):
     # TODO : compléter la valeur
-    raw_data_path = ""
+    raw_data_path = "/data/g9/raw"
 
     url = f'https://opendata.paris.fr/api/v2/catalog/datasets/comptages-routiers-permanents/exports/csv?refine=t_1h%3A{year}%2F{month}%2F{day}&timezone=UTC'
     r = requests.get(url, allow_redirects=True)
@@ -40,14 +40,18 @@ download_raw_data = download_raw_data(YEAR, MONTH, YESTERDAY)
 clean_data = BashOperator(
     task_id="spark_job_clean",
     # TODO : compléter le spark submit
-    bash_command="",
+    class_name = "\"Main\""
+    jar_path = "hdfs:///jars/g9/scala-clean-0.1.jar"
+    bash_command=f"spark-submit --deploy-mode cluster --class {class_name} --master yarn-cluster {jar_path}",
     dag=dag
 )
 
 transform_data = BashOperator(
     task_id="spark_job_transform",
     # TODO : compléter le spark submit
-    bash_command=f"",
+    class_name = "\"Main\""
+    jar_path = "hdfs:///jars/g9/scala-jointure-0.1.jar"
+    bash_command=f"spark-submit --deploy-mode cluster --class {class_name} --master yarn-cluster {jar_path}",
     dag=dag
 )
 
